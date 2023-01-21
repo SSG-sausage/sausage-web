24d05da1dd7db766fd7250fda72cdf95b0bbf7cf
Joon <orijoon98@gmail.com>
2023-01-21 22:34:35 +0900

    [TERK-77/Fix] 공유장바구니 관련 api 통신 수정

--- src/api/cartshare/cartShare.js
+++ src/api/cartshare/cartShare.js
@@ -1,15 +1,23 @@
-import axios from 'axios';
+import { axiosInstance } from '../instance';
 
-export const findCartShareList = async () =>
-    await axios.get('/api/cart-share', {
-        headers: {
-            mbrId: 1,
-        },
-    });
+export const findCartShareList = mbrId =>
+    axiosInstance
+        .get('/cart-share/api/cart-share', {
+            headers: {
+                mbrId: mbrId,
+            },
+        })
+        .catch(() => {
+            alert('공유장바구니 리스트 조회 실패');
+        });
 
-export const findCartShare = async cartShareId =>
-    await axios.get(`/api/cart-share/${cartShareId}`, {
-        headers: {
-            mbrId: 1,
-        },
-    });
+export const findCartShare = (mbrId, cartShareId) =>
+    axiosInstance
+        .get(`/cart-share/api/cart-share/${cartShareId}`, {
+            headers: {
+                mbrId: mbrId,
+            },
+        })
+        .catch(() => {
+            alert('공유장바구니 조회 실패');
+        });
